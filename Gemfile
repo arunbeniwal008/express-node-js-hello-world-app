266296adc778209d1c450dc57614f9c798cb16a1
arunbeniwal008 <arunbeniwal2010@gmail.com>
2023-02-09 08:33:44 +0000

    first

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+# Gems go here
+group :development do
+# Deployment
+gem "capistrano", "~> 3.10", require: false
+gem "capistrano-rails", "~> 1.3", require: false
+end
---
+++ node-server-restart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Stopping existing node servers
+echo "Stopping any existing node servers"
+pkill node
+cd /home/deploy/hello-world/current
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in path now)
+#install node modules
+npm install
+#start our node app in the background
+node server.js > app.out.log 2> app.err.log < /dev/null &
